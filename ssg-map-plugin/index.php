524767835d1e73b8fd165ba607694726df79979a
Marc96GER <marc.re@gmx.de>
2021-11-09 11:16:34 +0100

    Update index.php

--- ssg-map-plugin/index.php
+++ ssg-map-plugin/index.php
@@ -4,7 +4,7 @@
  * Description: Explore your city
  * Plugin URI: http://localhost
  * Version 1.0
- * Auther: Magnus Olesen & Amanda Kristena Jakse
+ * Auther: Magnus Olesen, Amanda Kristena Jakse, Marc Hunsicker
  * Auther http://localhost
  * License: GPL2
  */
